aec1378853873d6b0f04f90d38ac5ab59494d0aa
Niraj Khankari <156000340+NirajKhankari30@users.noreply.github.com>
2025-07-17 09:20:46 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,20 @@
+def sum(a,b):
+    c = a + b
+    return c
+    
+def sub(a,b):
+    c = a - b
+    return c
+    
+def mul(a,b):
+    c = a * b
+    return c
+    
+def div(a,b):
+    c = a / b
+    return c
+    
+def exp(a,b):
+    c = a ** b
+    return c
+
